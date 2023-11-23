5af6d1a7ad6b83e097c2c8bfdf7d3091b1605784
prashant-kiit <71011242+prashant-kiit@users.noreply.github.com>
2023-11-23 11:05:52 +0530

    Add files via upload

---
+++ addressal.js
@@ -0,0 +1,16 @@
+let flag = 1;
+let colorName = "blue"; 
+function addressal() {
+    if (flag==1) {
+        document.getElementById("address_").innerHTML = "Aliens! Hello";
+        document.querySelector(".address").style.color = colorName;
+        flag = 0;
+        colorName = "green"; 
+    }
+    else {
+        document.getElementById("address_").innerHTML = "Hello Aliens!";
+        document.querySelector(".address").style.color = colorName;
+        flag = 1;
+        colorName = "blue"; 
+    }
+}
